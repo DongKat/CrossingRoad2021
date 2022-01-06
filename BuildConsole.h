60ab600a82a942c0b4bfab8d6e89e18aab8cb26e
davidle2810 <81604200+davidle2810@users.noreply.github.com>
2022-01-06 14:49:14 +0700

    Add files via upload

---
+++ BuildConsole.h
@@ -0,0 +1,18 @@
+#ifndef console
+#define console
+#include <Windows.h>
+#include <iostream>
+
+using namespace std;
+
+#define CONSOLE_WIDTH 200
+#define	CONSOLE_HEIGHT 30
+
+void ShowConsoleCursor(bool showFlag);
+void Resize(int, int);
+void FixConsoleWindow();
+void TextColor(int x);
+void GotoXy(int x, int y);
+void clrscr();
+void drawBoard(int, int, int, int);
+#endif
