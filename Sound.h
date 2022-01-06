260088179a02c5aea153a1c7b102b75aa2559cdd
davidle2810 <81604200+davidle2810@users.noreply.github.com>
2022-01-06 14:24:47 +0700

    Add files via upload

---
+++ Sound.h
@@ -0,0 +1,25 @@
+#ifndef sound
+#define sound
+#include <Windows.h>
+#include <mmsystem.h>
+#include <string>
+#include <iostream>
+#pragma comment(lib, "Winmm.lib")
+
+using namespace std;
+
+class Sound
+{
+private:
+	bool turn; 
+public:
+	Sound() 
+	{
+		turn = true;
+	}
+	bool getState() { return turn; }
+	void switchState() { turn = !turn; }
+	void play(string path);
+	void stop(string path);
+};
+#endif
