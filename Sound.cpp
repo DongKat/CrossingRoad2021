d91f6749ee2bfcb9595cd216c691e254eb79b480
Dong Cat <hbdcat20@apcs.vn>
2022-01-06 13:01:59 +0700

    mciSendString -> PlaySound

--- Sound.cpp
+++ Sound.cpp
@@ -13,23 +13,23 @@ void SoundGame::turnOff()
 void SoundGame::play(string path)
 {
 	string dir = "play " + path + " repeat";
-	mciSendString(dir.c_str(), NULL, 0, NULL);
+	PlaySound(path), NULL, SND_FILENAME | SND_LOOP | SND_ASYNC);
 }
 
 void SoundGame::stop(string path)
 {
 	string dir = "close " + path;
-	mciSendString(dir.c_str(), NULL, 0, NULL);
+	PlaySound(path, NULL, SND_FILENAME | SND_LOOP | SND_ASYNC);
 }
 
-void SoundGame::pause(string path)
-{
-	string dir = "pause " + path;
-	mciSendString(dir.c_str(), NULL, 0, NULL);
-}
-
-void SoundGame::resume(string path)
-{
-	string dir = "resume " + path;
-	mciSendString(dir.c_str(), NULL, 0, NULL);
-}
+//void SoundGame::pause(string path)
+//{
+//	string dir = "pause " + path;
+//	mciSendString(dir.c_str(), NULL, 0, NULL);
+//}
+//
+//void SoundGame::resume(string path)
+//{
+//	string dir = "resume " + path;
+//	mciSendString(dir.c_str(), NULL, 0, NULL);
+//}
